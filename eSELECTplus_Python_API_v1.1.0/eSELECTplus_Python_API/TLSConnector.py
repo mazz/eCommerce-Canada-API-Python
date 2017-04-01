056f8537382cf20ae052e589fb4d896b29262035
Michael Hanna <mykelkim@yahoo.com>
2017-04-01 18:45:00 -0400

    add tls1.1 support, not tested

--- eSELECTplus_Python_API_v1.1.0/eSELECTplus_Python_API/TLSConnector.py
+++ eSELECTplus_Python_API_v1.1.0/eSELECTplus_Python_API/TLSConnector.py
@@ -31,6 +31,32 @@ class TLS1Handler(urllib.request.HTTPSHandler):
     def https_open(self, req):
         return self.do_open(TLS1Connection, req)
 
+class TLS1_1Connection(http.client.HTTPSConnection):
+    """Like HTTPSConnection but more specific"""
+    def __init__(self, host, **kwargs):
+        http.client.HTTPSConnection.__init__(self, host, **kwargs)
+
+    def connect(self):
+        """Overrides HTTPSConnection.connect to specify TLS version"""
+        # Standard implementation from HTTPSConnection, which is not
+        # designed for extension, unfortunately
+        sock = socket.create_connection((self.host, self.port),
+                self.timeout, self.source_address)
+        if getattr(self, '_tunnel_host', None):
+            self.sock = sock
+            self._tunnel()
+
+        # This is the only difference; default wrap_socket uses SSLv23
+        self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file,
+                ssl_version=ssl.PROTOCOL_TLSv1_1)
+
+class TLS1_1Handler(urllib.request.HTTPSHandler):
+    """Like HTTPSHandler but more specific"""
+    def __init__(self):
+        urllib.request.HTTPSHandler.__init__(self)
+
+    def https_open(self, req):
+        return self.do_open(TLS1_1Connection, req)
 
 class TLS1_2Connection(http.client.HTTPSConnection):
     """Like HTTPSConnection but more specific"""
